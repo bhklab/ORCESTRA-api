2d5ed55f0232e619bf8e17c3e36d18a08f38e140
Jermiah Joseph <jermiahjoseph98@gmail.com>
2024-06-19 12:37:06 -0400

    feat: use local mongo when developing

--- src/orcestrator/db.py
+++ src/orcestrator/db.py
@@ -12,10 +12,10 @@ async def get_db() -> AsyncIOMotorDatabase:
     global db_client
 
     db_name = settings.MONGO_DB
-    if db_name is None:
+    if not db_name or db_name == "":
         raise ValueError("MONGO_DB is not set")
-
-    if db_client is None:
+    logger.debug(f"db_name: {db_name}")
+    if not db_client:
         raise ValueError("Mongo has not been connected")
 
     return db_client[db_name]
@@ -23,6 +23,9 @@ async def get_db() -> AsyncIOMotorDatabase:
 
 async def connect_and_init_db() -> None:
     global db_client
+
+    if not settings.MONGO_URI or settings.MONGO_URI == "":
+        raise ValueError("MONGO_URI is not set")
     try:
         db_client = AsyncIOMotorClient(settings.MONGO_URI)
         logger.info("Connected to mongo.")
